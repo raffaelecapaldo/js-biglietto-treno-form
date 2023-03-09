1f2174245fc5b7401d129a49dab89400196876e7
raffaelecapaldo <raffaelecpl@gmail.com>
2023-03-09 16:00:40 +0100

    JS exercise completed

--- js/script.js
+++ js/script.js
@@ -21,9 +21,13 @@ const seniorOffer = 40;
 const maxAge = 130;
 let ticketPrice;
 const button = document.getElementById("buy-button");
+const htmlticket = document.getElementById("ticket-layout");
+const errorbox = document.getElementById("ok-error-box");
+const error = document.getElementById("ok-error");
+
 
 //ADD EVENT LISTENER FOR THE BUY BUTTON THAT CONTAIN THE CODE OF THIS EXERCISE
-button.addEventListener("click", function() {
+button.addEventListener("click", function () {
     //TAKE THE VALUE VARIABLES FROM THE FORM VALUE
     let nameUser = document.getElementById("user-name-input").value;
     let surnameUser = document.getElementById("user-surname-input").value;
@@ -35,8 +39,12 @@ button.addEventListener("click", function() {
     ageUser = parseInt(ageUser);
     kmRide = parseInt(kmRide);
     //CHECK ALL THE VALUES
-    if (nameUser && isNaN(nameUser) && surnameUser && isNaN(surnameUser) && ageUser && !isNaN(ageUser) && ageUser > 0 && kmRide && !isNaN(kmRide) && kmRide > 0 ) {
-        //INPUTS ARE OK, CALC RIDE PRICE
+    if (nameUser && isNaN(nameUser) && surnameUser && isNaN(surnameUser) && ageUser && !isNaN(ageUser) && ageUser > 0 && ageUser < maxAge && kmRide && !isNaN(kmRide) && kmRide > 0) {
+        //INPUTS ARE OK, IF BEFORE THEY WERE NOT, NOW REMOVE THE ERROR AND CHANGE WITH THE OK MESSAGE
+        errorbox.classList.remove("red");
+        errorbox.classList.add("green");
+        error.innerHTML = "Grazie per il tuo acquisto!"
+        // CALC RIDE PRICE
         ridePrice = kmPrice * kmRide;
         //APPLY THE OFFER BASED ON THE AGE
         if (ageUser < youngTarget) {
@@ -52,36 +60,46 @@ button.addEventListener("click", function() {
             ridePriceNew = ridePrice;
             document.getElementById("offer-type").innerHTML = "Base"
         }
-
-            //WRITE THE AGE IN THE TICKET html
-            document.getElementById("user-age").innerHTML = ageUser;
-            //WRITE RIDE KMs IN THE TICKET html
-            document.getElementById("ride-km").innerHTML = kmRide;
-            // MAKE THE PRICE IN EURO FORMAT (2 decimals)
-            ridePriceNew = ridePriceNew.toFixed(2);
-            // REPLACE DOT WITH COMMA (euro format)
-            ridePriceNew = ridePriceNew.replace(".", ",");
-            //ADD EURO SYMBOL 
-            ridePriceNew = ridePriceNew += "€";
-            //ADD ticket-price in the HTML
-            document.getElementById("ticket-price").innerHTML = ridePriceNew;
-            // WRITE BASE PRICE IN HTML ONLY IF OFFER IS APPLIED (EURO FORMATTED)
-            if (ageUser < youngTarget || (ageUser > seniorTarget)) {
+        //WRITE THE NAME IN THE TICKET
+        document.getElementById("owner-name").innerHTML = nameUser;
+        //WRITE THE SURNAME IN THE TICKET
+        document.getElementById("owner-surname").innerHTML = surnameUser;
+        //WRITE THE AGE IN THE TICKET html
+        document.getElementById("user-age").innerHTML = ageUser;
+        //WRITE RIDE KMs IN THE TICKET html
+        document.getElementById("ride-km").innerHTML = kmRide;
+        // MAKE THE PRICE IN EURO FORMAT (2 decimals)
+        ridePriceNew = ridePriceNew.toFixed(2);
+        // REPLACE DOT WITH COMMA (euro format)
+        ridePriceNew = ridePriceNew.replace(".", ",");
+        //ADD EURO SYMBOL 
+        ridePriceNew = ridePriceNew += "€";
+        //ADD ticket-price in the HTML
+        document.getElementById("ticket-price").innerHTML = ridePriceNew;
+        // WRITE BASE PRICE IN HTML ONLY IF OFFER IS APPLIED (EURO FORMATTED)
+        if (ageUser < youngTarget || (ageUser > seniorTarget)) {
             ridePrice = ridePrice.toFixed(2);
             ridePrice = ridePrice.replace(".", ",");
             ridePrice = ridePrice += "€";
             document.getElementById("base-price").innerHTML = ridePrice;
-}
-// OTHERWISE EMPTY THE DIV
-else {
-    document.getElementById("pricebase-box").innerHTML = "";
-}
+
+            //SHOW THE TICKET
+            htmlticket.classList.remove("d-none");
+        }
+        // OTHERWISE EMPTY THE DIV
+        else {
+            document.getElementById("pricebase-box").innerHTML = "";
+        }
 
 
     }
 
     else {
-        //INPUTS CHECK FAILED
-        document.getElementById("error").innerHTML = "I dati di acquisto non risultano validi, riprova.";
+        //INPUTS CHECK FAILED - ADD ERROR AND IF BEFORE THEY WERE OK REMOVE THE CLASS GREEN AND THE TICKET
+        errorbox.classList.remove("green");
+        errorbox.classList.add("red");
+        error.innerHTML = "I dati di acquisto non sono validi, riprova!"
+        htmlticket.classList.add("d-none");
+
     }
 })
